ea3124de6f11100b144185581c48249c4a450c07
unknown <seba.przyimka@gmail.com>
2024-04-17 22:38:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [name, setName] = useState('');
+	const [quantity, setQuantity] = useState(0);
+	const [itemsShopping, setItemsShopping] = useState([]);
+	const [text, setText] = useState('');
+
+	function setInput(e) {
+		setText(e.target.value);
+		setName(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const shoppingItems = [
+			{ name, quantity: 0, id: new Date() },
+			...itemsShopping,
+		];
+
+		setItemsShopping(shoppingItems);
+		setText('');
+	}
+
+	return (
+		<div className='container'>
+			<Header input={text} setInput={setInput} onSubmit={handleSubmit} />
+			<ShoppingList itemsShopping={itemsShopping} id={itemsShopping.id} />
+		</div>
+	);
 }
 
-export default App;
+function Header({ input, setInput, onSubmit }) {
+	return (
+		<>
+			<h1>Shopping List 🍎</h1>
+			<form>
+				<input
+					className='input-name'
+					type='text'
+					placeholder='Item Name'
+					value={input}
+					onChange={setInput}
+				/>
+				<input className='input-button' type='submit' onClick={onSubmit} />
+			</form>
+		</>
+	);
+}
+
+function ShoppingList({ itemsShopping }) {
+	return (
+		<div className='elements'>
+			<ul>
+				{itemsShopping.map(item => (
+					<Items name={item.name} items={item.quantity} key={item.id} />
+				))}
+			</ul>
+
+			<span className='total'>Total items </span>
+		</div>
+	);
+}
+
+function Items({ name, items }) {
+	return (
+		<>
+			<li>
+				<div>
+					<input className='checkbox' type='checkbox' /> {name}
+				</div>
+				<div>
+					<button>◀️</button>
+					<span>{items}</span>
+					<button>▶️</button>
+					<button className='delete'>🗑️</button>
+				</div>
+			</li>
+		</>
+	);
+}
