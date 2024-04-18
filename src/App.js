f00aef68050dfbe854aef0e0867ba86cb9182d60
Sebastian Przyimka <seba.przyimka@gmail.com>
2024-04-18 19:48:09 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,36 +2,65 @@ import { useState } from 'react';
 
 export default function App() {
 	const [name, setName] = useState('');
-	const [quantity, setQuantity] = useState(0);
+	const [quantity, setQuantity] = useState(1);
 	const [itemsShopping, setItemsShopping] = useState([]);
 	const [text, setText] = useState('');
 
-	function setInput(e) {
-		setText(e.target.value);
-		setName(e.target.value);
-	}
-
+	// if error put bad shopping items in handleSubmit function
+	const shoppingItems = [
+		...itemsShopping,
+		{ name: text, quantity: quantity, id: new Date() },
+	];
+	//
 	function handleSubmit(e) {
 		e.preventDefault();
-
-		const shoppingItems = [
-			{ name, quantity: 0, id: new Date() },
-			...itemsShopping,
-		];
 
 		setItemsShopping(shoppingItems);
 		setText('');
 	}
 
+	function handleDelete(id) {
+		setItemsShopping(deleteItems => deleteItems.filter(item => item.id !== id));
+	}
+
+	function handleItem(id, increment) {
+		setItemsShopping(prevItem =>
+			prevItem.map(item => {
+				if (item.id === id) {
+					return { ...item, quantity: item.quantity + increment };
+				}
+				return item;
+			})
+		);
+	}
+
+	function handleAddItems() {
+		return itemsShopping.reduce((total, item) => total + item.quantity, 0);
+	}
+
 	return (
 		<div className='container'>
-			<Header input={text} setInput={setInput} onSubmit={handleSubmit} />
-			<ShoppingList itemsShopping={itemsShopping} id={itemsShopping.id} />
+			<Header
+				text={text}
+				onSubmit={handleSubmit}
+				setText={setText}
+				setName={setName}
+				name={name}
+			/>
+			<ShoppingList
+				itemsShopping={itemsShopping}
+				id={itemsShopping.id}
+				onDelete={handleDelete}
+				quantity={quantity}
+				onHandleItem={handleItem}
+				setQuantity={quantity}
+				onAddItems={handleAddItems}
+			/>
 		</div>
 	);
 }
 
-function Header({ input, setInput, onSubmit }) {
+function Header({ text, onSubmit, setText }) {
 	return (
 		<>
 			<h1>Shopping List 🍎</h1>
@@ -40,8 +69,8 @@ function Header({ input, setInput, onSubmit }) {
 					className='input-name'
 					type='text'
 					placeholder='Item Name'
-					value={input}
-					onChange={setInput}
+					value={text}
+					onChange={e => setText(e.target.value)}
 				/>
 				<input className='input-button' type='submit' onClick={onSubmit} />
 			</form>
@@ -49,21 +78,34 @@ function Header({ input, setInput, onSubmit }) {
 	);
 }
 
-function ShoppingList({ itemsShopping }) {
+function ShoppingList({
+	itemsShopping,
+	onDelete,
+	onHandleItem,
+	onAddItems,
+	quantity,
+}) {
 	return (
 		<div className='elements'>
 			<ul>
 				{itemsShopping.map(item => (
-					<Items name={item.name} items={item.quantity} key={item.id} />
+					<Items
+						onDelete={onDelete}
+						name={item.name}
+						quantity={item.quantity}
+						id={item.id}
+						key={item.id}
+						onHandleItem={onHandleItem}
+					/>
 				))}
 			</ul>
 
-			<span className='total'>Total items </span>
+			<span className='total'>Total items {onAddItems()}</span>
 		</div>
 	);
 }
 
-function Items({ name, items }) {
+function Items({ name, quantity, onDelete, id, onHandleItem }) {
 	return (
 		<>
 			<li>
@@ -71,10 +113,19 @@ function Items({ name, items }) {
 					<input className='checkbox' type='checkbox' /> {name}
 				</div>
 				<div>
-					<button>◀️</button>
-					<span>{items}</span>
-					<button>▶️</button>
-					<button className='delete'>🗑️</button>
+					<button
+						className='arrow'
+						onClick={() => onHandleItem(id, quantity >= 1 ? -1 : 0)}
+					>
+						◀️
+					</button>
+					<span>{quantity}</span>
+					<button className='arrow' onClick={() => onHandleItem(id, 1)}>
+						▶️
+					</button>
+					<button onClick={() => onDelete(id)} className='delete'>
+						🗑️
+					</button>
 				</div>
 			</li>
 		</>
